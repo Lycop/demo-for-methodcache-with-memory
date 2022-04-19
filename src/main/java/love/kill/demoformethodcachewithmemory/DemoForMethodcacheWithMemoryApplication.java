2f9273f1eacf3324deae3a8c5b2a89b677c3ab0f
Lycop <i@kill.love>
2022-04-19 19:24:57 +0800

    demo for methodcache with memory

---
+++ src/main/java/love/kill/demoformethodcachewithmemory/DemoForMethodcacheWithMemoryApplication.java
@@ -0,0 +1,12 @@
+package love.kill.demoformethodcachewithmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForMethodcacheWithMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForMethodcacheWithMemoryApplication.class, args);
+	}
+}
---
+++ src/main/java/love/kill/demoformethodcachewithmemory/service/DemoService.java
@@ -0,0 +1,14 @@
+package love.kill.demoformethodcachewithmemory.service;
+
+/**
+ *
+ *
+ * @author Lycop
+ */
+public interface DemoService {
+
+	String getDataWithoutMethodCache();
+
+	String getDataWithMethodCache();
+
+}
